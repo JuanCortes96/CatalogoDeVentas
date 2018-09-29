d5feb80399237b0bbee58452a52ea5c5ba19fe34
Óscar Zamora <oscar@macbook-de-oscar.local>
2018-09-28 23:45:49 -0500

    Initial Commit

---
+++ CatalogoDeVentas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CatalogoDeVentas
+//
+//  Created by Óscar Zamora on 9/28/18.
+//  Copyright © 2018 mpoo03. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
